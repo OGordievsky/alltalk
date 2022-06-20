ff8cb121e9400ce7362d11639cd8c531c757c415
OGordievsky <og87@bk.ru>
2022-06-20 10:10:14 +0300

    init git

---
+++ src/test/java/ru/forgottendev/alltalk/AllTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.forgottendev.alltalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
